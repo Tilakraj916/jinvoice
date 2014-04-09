127b81fc810dc1d69190dcc593e1198e0c9954de
Sergio Pino <pino.sergio@richemont.com>
2014-04-10 01:48:43 +0200

    Initial version based on appfuse 3.0

---
+++ core/src/main/java/com/sergiopino/service/impl/PasswordTokenManager.java
@@ -0,0 +1,19 @@
+package com.sergiopino.service.impl;
+
+import com.sergiopino.model.User;
+
+
+public interface PasswordTokenManager {
+
+    /**
+     * {@inheritDoc}
+     */
+    String generateRecoveryToken(User user);
+
+    /**
+     * {@inheritDoc}
+     */
+    boolean isRecoveryTokenValid(User user, String token);
+
+    void invalidateRecoveryToken(User user, String token);
+}
